cd3b17001f93cb30159f9644894b8dc195471ad9
blgx777 <85019907+blgx777@users.noreply.github.com>
2021-12-26 15:10:06 +0100

    Update filters.js

--- plugins/filters.js
+++ plugins/filters.js
@@ -144,8 +144,8 @@ Asena.addCommand({on: 'text', fromMe: false}, (async (message, match) => {
     if(Config.AUTOSTICKER){
     let banned = jid.find( Jid => Jid === message.jid);
     if(banned !== undefined) return
-    if (!!message.mention && message.mention[0] == '919946432377@s.whatsapp.net') {
-await message.client.sendMessage(message.jid, fs.readFileSync('./sticker/song.webp'), MessageType.sticker, { mimetype: Mimetype.webp, quoted : message.data, ptt: false})
+    if (!!message.mention && message.mention[0] == '212679184660@s.whatsapp.net') {
+await message.client.sendMessage(message.jid, fs.readFileSync('./uploads/Welcome.mp3'), MessageType.sticker, { mimetype: Mimetype.webp, quoted : message.data, ptt: false})
     }
 const array = ['BELGHIRIA,'BLG','NEGRO','ILYASS','ZAHYA','PAUSE','put','PAUSE DZB','MORPHINE','Welcome','LONELY','LAYLAHAYLALAH','HOUSSAM','ROUND','diib','2bacpc2']
 array.map( async (a) => {
